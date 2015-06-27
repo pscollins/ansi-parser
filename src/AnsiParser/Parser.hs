9e50166efe3e68b6ccf0ee1d81d8ff93d920a983
pscollins <pscollins@uchicago.edu>
2015-06-27 16:38:33 -0500

    giving up on fancy string typeclasses

--- src/AnsiParser/Parser.hs
+++ src/AnsiParser/Parser.hs
@@ -1,38 +1,50 @@
 {-# LANGUAGE OverloadedStrings, FlexibleContexts  #-}
 module AnsiParser.Parser where
 
+import AnsiParser.Types
+
 import Text.ParserCombinators.Parsec.Token
 import Text.Parsec
 import Text.Parsec.Char (oneOf, satisfy)
 import Text.Parsec.ByteString (Parser)
 
-import Data.ByteString.Char8 (ByteString)
+import Data.String (IsString)
+-- import Data.ByteString.Char8 (ByteString)
 
 import Data.Char (isControl)
 
-escapeCode :: (Stream s m Char) => ParsecT s u m Char
-escapeCode = oneOf "\x027"
 
-commandBody :: (Stream s m Char) => ParsecT s u m Char
-commandBody = satisfy $ not . isControl
 
--- TODO: set reservedNames to hold the single-character codes
-ansiTokenParser :: Stream s m Char => GenTokenParser s u m
-ansiTokenParser = makeTokenParser $
-                  LanguageDef { commentStart = ""
-                              , commentEnd = ""
-                              , commentLine = ""
-                              , nestedComments = False
-                              , identStart = parserZero
-                              , identLetter = parserZero
-                              , opStart = escapeCode
-                              , opLetter = commandBody
-                              , reservedNames = []
-                              , reservedOpNames = []
-                              , caseSensitive = True }
+-- isEscapeCode :: IsString a => a -> Bool
+-- isEscapeCode = (`elem` "\x027")
 
-plainText :: Parser ByteString
-plainText = undefined
+-- TODO: Other option here?
+escapeCode :: (ConsoleString a, Stream s m a) => ParsecT s u m a
+escapeCode = satisfy isEscape
+
+-- commandBody :: (Stream s m Char) => ParsecT s u m Char
+-- commandBody = satisfy $ not . isControl
+
+-- -- TODO: set reservedNames to hold the single-character codes
+-- ansiTokenParser :: (IsString a, Stream s m a) => GenTokenParser s u m
+-- ansiTokenParser = makeTokenParser $
+--                   LanguageDef { commentStart = ""
+--                               , commentEnd = ""
+--                               , commentLine = ""
+--                               , nestedComments = False
+--                               , identStart = parserZero
+--                               , identLetter = parserZero
+--                               , opStart = escapeCode
+--                               , opLetter = commandBody
+--                               , reservedNames = []
+--                               , reservedOpNames = []
+--                               , caseSensitive = True }
+
+-- plainText :: Parser (Expr a)
+-- plainText = do
+--   body <- manyTill anyToken escapeCode
+--   return $ Plain body
+
 
 -- tokenParser :: Stream s m Char => GenLanguageDef s u m -> GenTokenParser s u m
 -- tokenParser = makeTokenParser
--- src/AnsiParser/Types.hs
+++ src/AnsiParser/Types.hs
@@ -1,41 +1,59 @@
+{-# LANGUAGE FlexibleInstances  #-}
 module AnsiParser.Types where
 
-data Color
-  = Black
-  | Red
-  | Green
-  | Brown
-  | Blue
-  | Magenta
-  | Cyan
-  | White
-  deriving (Show, Eq)
+import qualified Data.ByteString.Char8 as B
+import Data.String (IsString)
 
-data ColorPos
-  = Foreground
-  | Background
-  deriving (Show, Eq)
+-- This lets us put off the decision about how we'll represent our characters
+class IsString a => ConsoleString a where
+  isEscape :: a -> Bool
+  -- printable :: a ->
 
-data ColorCmd
-  = Reset -- other stuff to, TODO
-  | Bold
-  | Set (Color, ColorPos)
-  deriving (Show, Eq)
+instance ConsoleString [Char] where
+  isEscape = ('\x27' `elem`)
 
--- Here, we will handle "ESC ] 0" (set both icon name + window title)
--- by replacing it with two separate commands.
-data OSCmd
-  = IconName String
-  | WindowTitle String
-  deriving (Show, Eq)
-  -- Not supported:
-  -- ColorOverride Integer String
-  -- DynamicTextColor String
-  -- Font String
+instance ConsoleString B.ByteString where
+  isEscape = ('\x27' `B.elem`)
 
-data DisplayExpr
-  = Text String
-  | CSI Char [String] -- Control Sequence (Initiator)
-  | SGR [ColorCmd] -- Select Graphic Rendition i.e. colors, fonts
-  | OSC OSCmd -- Operating System Command
-  deriving (Show, Eq)
+data ConsoleString a => Expr a
+  = Plain a
+
+-- data Color
+--   = Black
+--   | Red
+--   | Green
+--   | Brown
+--   | Blue
+--   | Magenta
+--   | Cyan
+--   | White
+--   deriving (Show, Eq)
+
+-- data ColorPos
+--   = Foreground
+--   | Background
+--   deriving (Show, Eq)
+
+-- data ColorCmd
+--   = Reset -- other stuff to, TODO
+--   | Bold
+--   | Set (Color, ColorPos)
+--   deriving (Show, Eq)
+
+-- -- Here, we will handle "ESC ] 0" (set both icon name + window title)
+-- -- by replacing it with two separate commands.
+-- data OSCmd
+--   = IconName String
+--   | WindowTitle String
+--   deriving (Show, Eq)
+--   -- Not supported:
+--   -- ColorOverride Integer String
+--   -- DynamicTextColor String
+--   -- Font String
+
+-- data DisplayExpr
+--   = Text String
+--   | CSI Char [String] -- Control Sequence (Initiator)
+--   | SGR [ColorCmd] -- Select Graphic Rendition i.e. colors, fonts
+--   | OSC OSCmd -- Operating System Command
+--   deriving (Show, Eq)
